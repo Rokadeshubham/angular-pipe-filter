e29dd4266ad160fe90fe1f653aa36c3762b3e42d
Rokadeshubham <shubhamrokade211@gmail.com>
2019-08-14 10:33:13 +0530

    1 file modified

--- src/app/employee-filter.pipe.ts
+++ src/app/employee-filter.pipe.ts
@@ -7,10 +7,9 @@ import { AppComponent } from './app.component';
 
 export class EmployeeFilterPipe implements PipeTransform{
   transform(items: any[], searchText: string): any[] {
-  if(!items) return [];
-  if(!searchText) return items;
-  searchText = searchText.toLowerCase();
-      return items.filter( it => {return it.toLowerCase().includes(searchText);
-    });
+          if(!items) return [];
+          if(!searchText) return items;
+          searchText = searchText.toLowerCase();
+          return items.filter( it => {return it.toLowerCase().includes(searchText);});
    }
 }
